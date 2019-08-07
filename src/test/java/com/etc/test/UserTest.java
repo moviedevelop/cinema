df565d4508685f5d5b27e993d3eba439a941b1fe
xc <2443100564@qq.com>
2019-08-07 09:18:22 +0800

    功能点测试

---
+++ src/test/java/com/etc/test/UserTest.java
@@ -0,0 +1,29 @@
+package com.etc.test;
+
+import com.etc.dao.UsersMapper;
+import com.etc.pojo.Users;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UserTest {
+    private ApplicationContext context;
+    private UsersMapper mapper;
+
+    @Before
+    public void setUp(){
+        context = new ClassPathXmlApplicationContext(new String[]{"spring-dao.xml","spring-service.xml"});
+        mapper = context.getBean("usersMapper",UsersMapper.class);
+    }
+
+    @Test
+    public void testInsert(){
+        Users users = new Users();
+        users.setUserName("殇");
+        users.setUserPass("123456");
+        users.setUserTel("15387900663");
+        users.setUserAbout("牛的一批");
+        mapper.insert(users);
+    }
+}
